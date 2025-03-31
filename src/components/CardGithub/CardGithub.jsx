9499c15168c2c901ecc60f1e65e49dbec0f4c633
Alisson Souza <alissonso432@gmail.com>
2025-03-31 09:25:18 -0300

    adicionando estilização noCardGithub

--- src/components/CardGithub/CardGithub.jsx
+++ src/components/CardGithub/CardGithub.jsx
@@ -1,6 +1,9 @@
 import { useState } from 'react';
 import './CardGithub.css';
 import logo from '../../../public/logo_github.svg';
+import luz_azul from '../../../public/luz_azul_esquerda.svg'
+import luz_azul_ from '../../../public/luz_azul_direita.svg'
+import quadro_pontilhado from '../../../public/quadro_pontilhado.svg'
 import { CardPerfil } from '../CardPerfil/CardPerfil';
 
 export function CardGithub() {
@@ -16,32 +19,38 @@ export function CardGithub() {
 
     return (
         <>
-            <div className='card_github'>
-                <div className='container-logo'>
-                    <img src={logo} alt="logo" className='logo' />
-                    <span>Perfil <strong>Github</strong></span>
-                </div>
-                <nav className="navbar ">
-                    <div className="container-fluid">
-                        <form className="d-flex " role="search" onSubmit={handleSubmit}>
-                            <div className="input">
-                                <input
-                                    className="form-control"
-                                    type="search"
-                                    placeholder="Digite um usuário do Github"
-                                    value={usuario}
-                                    onChange={(e) => setUsuario(e.target.value)}
-                                />
-                                <button 
-                                    className="btn btn-primary" 
-                                    type="submit"
-                                ></button>
-                            </div>
-                        </form>
+            <div className='container-github'>
+                <img src={luz_azul} alt="luz" className='luz_azul_esquerda'/>
+                <img src={luz_azul_} alt="luz" className='luz_azul_direita'/>
+                <img src={quadro_pontilhado} alt="quadro" className='quadro_pontilhado'/>
+                <div className='card_github'>
+                    <div className='container-logo'>
+                        <img src={logo} alt="logo" className='logo' />
+                        <span>Perfil <strong>Github</strong></span>
                     </div>
-                </nav>
-                {mostrarPerfil && <CardPerfil usuarioGitHub={usuarioPesquisado} />}
-            </div>  
+                    <nav className="navbar ">
+                        <div className="container-fluid">
+                            <form className="d-flex " role="search" onSubmit={handleSubmit}>
+                                <div className="input">
+                                    <input
+                                        className="form-control"
+                                        placeholder="Digite um usuário do Github"
+                                        value={usuario}
+                                        onChange={(e) => setUsuario(e.target.value)}
+                                    />
+                                    <button 
+                                        className="btn btn-primary" 
+                                        type="submit"
+                                    ></button>
+                                </div>
+                            </form>
+                        </div>
+                    </nav>
+                    {mostrarPerfil && <CardPerfil usuarioGitHub={usuarioPesquisado} />}
+                </div>  
+
+
+            </div>
         </>
     );
 };
